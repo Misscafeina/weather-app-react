c95ca3c19ffdbf77c66ac81fceb5751501474f87
Isabel PM <ispemaz@gmail.com>
2023-11-29 15:35:52 +0100

    Eliminadas las hojas de estilo no utilizadas, modificados archivos JS y CSS, y agregada una nueva imagen de clima (weather.JPG).

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  //const url='https://api.openweathermap.org/data/2.5/weather?q=mijas&appid=42171026e534ffc7a13395ee06964088'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Mijas</p>
+          </div>
+          <div className="temp">
+            <h1>18 °C</h1>
+          </div>
+          <div className="Clouds"></div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="feels">
+          <p>19 °C</p>
+        </div>
+        <div className="humidity">
+          <p>74%</p>
+        </div>
+        <div className="wind">
+          <p>14 km/h</p>
+        </div>
+      </div>
     </div>
   );
 }
